43140cd48dfb56ea75402f23be09183a1959d77e
reiji <31179220+reud@users.noreply.github.com>
2019-02-10 20:53:29 +0900

    css in html

--- tests/views.py
+++ tests/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from django.views.decorators.clickjacking import xframe_options_exempt
 
 # Create your views here.
+@xframe_options_exempt
 def atcoder_badge(request):
     return render(request, 'tests/AtCoderStatuses/badge.html')
