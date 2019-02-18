d64bf565ce79e1de0368ba1f5edb4ee3f866950f
reiji <31179220+reud@users.noreply.github.com>
2019-02-18 19:28:23 +0900

    add new Grrrreeeeaaaaatttttttt Apppppp!!!

--- ramiel/views.py
+++ ramiel/views.py
@@ -2,20 +2,14 @@ from linebot import LineBotApi, WebhookHandler
 from django.http import HttpResponseForbidden, HttpResponse
 from linebot.exceptions import InvalidSignatureError
 import os
-# 各クライアントライブラリのインスタンス作成
 line_bot_api = LineBotApi(channel_access_token=os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(channel_secret=os.environ['CHANNEL_SECRET'])
 
 def callback(request):
-    # リクエストヘッダーから署名検証のための値を取得
     signature = request.META['HTTP_X_LINE_SIGNATURE']
-    # リクエストボディを取得
     body = request.body.decode('utf-8')
     try:
-        # 署名を検証し、問題なければhandleに定義されている関数を呼び出す
         handler.handle(body, signature)
     except InvalidSignatureError:
-        # 署名検証で失敗したときは例外をあげる
         HttpResponseForbidden()
-    # handleの処理を終えればOK
     return HttpResponse('OK', status=200)
