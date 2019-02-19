22bd81278aab0347e600145a418374701fd39cd1
reiji <31179220+reud@users.noreply.github.com>
2019-02-19 18:21:27 +0900

    homepage nokousin

--- reudme/settings.py
+++ reudme/settings.py
@@ -29,7 +29,7 @@ if 'charlotte.local' in HOSTNAME:
 else:
     SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
