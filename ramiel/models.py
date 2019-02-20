383664496f4730ad1607b9b47f0a1495deec2ce1
reiji <31179220+reud@users.noreply.github.com>
2019-02-20 10:09:49 +0900

    homepage nokousin

--- ramiel/models.py
+++ ramiel/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Vocabulary(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, null=True, blank=True)
+    author_line_id = models.CharField(null=True, blank=True)
+    serif = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    use_time = models.DateTimeField()
+    STATE = (
+        ('SENDED', '送信済み'),
+        ('WAITING', '未送信'),
+        ('DRAFT', '下書き'),
+    )
+    state = models.CharField(max_length=1, choices=STATE)
+
+    def __str__(self):
+        return self.serif
