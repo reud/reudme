56720088bf0ec707d5ec485f442d845dea5f1d1a
reiji <31179220+reud@users.noreply.github.com>
2019-03-02 16:13:36 +0900

    modelsを修正

--- ramiel/views.py
+++ ramiel/views.py
@@ -31,7 +31,8 @@ else:
     notifer = PyLineNotify.Notifer(notify_token=os.environ['LINE_NOTIFY_TOKEN'])
 
 # docomo api setting
-docomo_api_url = f'https://api.apigw.smt.docomo.ne.jp/naturalChatting/v1/?APIKEY={docomo_api_key}'
+docomo_communication_api_url = f'https://api.apigw.smt.docomo.ne.jp/naturalChatting/v1/dialogue?APIKEY={docomo_api_key}'
+docomo_user_register_api_url = f'https://api.apigw.smt.docomo.ne.jp/naturalChatting/v1/registration?APIKEY={docomo_api_key}'
 docomo_api_headers = {'Content-type': 'application/json', 'charset': 'UTF-8'}
 
 
@@ -49,7 +50,11 @@ def callback(request):
 @handler.add(FollowEvent)
 def handle_follow(event):
     profile = line_bot_api.get_profile(event.source.user_id)
-    LINEUser.objects.create(username=profile.display_name, line_id=profile.user_id)
+    payload = {'botId': "Chatting", 'appKind': 'ramiel_project'}
+    req = requests.post(docomo_communication_api_url, data=json.dumps(payload), headers=docomo_api_headers)
+    notifer.send_message(req)
+    response = req.json()
+    LINEUser.objects.create(username=profile.display_name, line_id=profile.user_id, app_id=response['appId'])
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=f'ようこそ！{profile.display_name}さん！')
@@ -58,27 +63,28 @@ def handle_follow(event):
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text='DataBaseにあなたの名前がありません。友達追加をお手数ですが再度行って下さい。よろしくお願い致します。'))
-    return
-    
     profile = line_bot_api.get_profile(event.source.user_id)
     user_object = LINEUser.objects.filter(line_id=profile.user_id).first()
     if user_object:
-        payload = {'utt': event.message.text, 'context': ''}
-        if user_object.context_id:
-            payload['context'] = user_object.context_id
-
+        payload = {'language': 'ja-JP',
+                   'botId': 'Chatting',
+                   'appId': user_object.app_id,
+                   'voiceText': event.message.text,
+                   'clientData': {
+                       'option': {
+                           "nickname": profile.display_name
+                       }
+                   },
+                   "appRecvTime": "2019-03-02 22:22:22",
+                   "appSendTime": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                   }
         # error point
-        req = requests.post(docomo_api_url, data=json.dumps(payload), headers=docomo_api_headers)
+        req = requests.post(docomo_communication_api_url, data=json.dumps(payload), headers=docomo_api_headers)
         notifer.send_message(req)
         print(req)
         res = req.json()
-        make_vocabulary(profile.user_id, res['utt'],
+        make_vocabulary(profile.user_id, res['systemText']['expression'],
                         datetime.datetime.now() + datetime.timedelta(minutes=int(random.random() * 30)))
-        user_object.context_id = res['context']
-        user_object.save()
     else:
         line_bot_api.reply_message(
             event.reply_token,
